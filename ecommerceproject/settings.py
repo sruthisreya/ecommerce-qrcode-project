97ab9cd4fdd4a9dc27a0a46208d9d35375c1efdd
sruthisreya <sreyamaya84@gmail.com>
2025-01-10 10:47:45 +0530

    removed pycache file

--- ecommerceproject/settings.py
+++ ecommerceproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,7 +140,7 @@ REST_FRAMEWORK = {
 
 
 
-from datetime import timedelta
+
 
 SIMPLE_JWT={
     'ACCESS_TOKEN_LIFETIME':timedelta(minutes=30),
