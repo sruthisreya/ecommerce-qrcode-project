0c964c6725a4788052eca039fd1ee9429be443aa
sruthisreya <sreyamaya84@gmail.com>
2025-02-11 11:51:43 +0530

    fix:created readme file

--- ecommerceapp/urls.py
+++ ecommerceapp/urls.py
@@ -9,24 +9,24 @@ app_name = 'package'
 
 
 urlpatterns = [
-    path('RegisterUser/', RegisterUser.as_view(),name='RegisterUser'),
-    path('ProtectedView/', ProtectedView.as_view(),name='ProtectedView'),
+    path('RegisterUser/', RegisterUser.as_view(), name='RegisterUser'),
+    path('ProtectedView/', ProtectedView.as_view(), name='ProtectedView'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token-btain-pair'),
     # path('token/',TokenObtainPairView.as_view(),name='token'),
-    path('refresh/',TokenRefreshView.as_view(),name='refresh'),
+    path('refresh/',TokenRefreshView.as_view(), name='refresh'),
     path('create-url/', create_50_urls, name='create-url'),
-    path('UniqueurlView/',UniqueurlView.as_view(),name='UniqueurlView'),
-    path('OpencartView/',OpencartView.as_view(),name='OpencartView'),
-    path('ContactQueryView/',ContactQueryView.as_view(),name='ContactQueryView'),
-    path('AddurlsTocartView/',AddurlsTocartView.as_view(),name='AddurlsTocartView'),
-    path('DeleteurlView/',DeleteurlView.as_view(),name='DeleteurlView'),
-    path('Paymentcreateview/',Paymentcreateview.as_view(),name='Paymentcreateview'),
-    path('payment-success/',PaymentSuccessView.as_view(),name='paymentsuccess'),
-    path('payment/cancel/',PaymentCancelView.as_view(),name='paymentcancel'),
+    path('UniqueurlView/', UniqueurlView.as_view(), name='UniqueurlView'),
+    path('OpencartView/', OpencartView.as_view(), name='OpencartView'),
+    path('ContactQueryView/', ContactQueryView.as_view(), name='ContactQueryView'),
+    path('AddurlsTocartView/', AddurlsTocartView.as_view(), name='AddurlsTocartView'),
+    path('DeleteurlView/', DeleteurlView.as_view(),name='DeleteurlView'),
+    path('Paymentcreateview/', Paymentcreateview.as_view(), name='Paymentcreateview'),
+    path('payment-success/', PaymentSuccessView.as_view(), name='paymentsuccess'),
+    path('payment/cancel/', PaymentCancelView.as_view(), name='paymentcancel'),
     # path('url/<str:url_id>/upload-image/',ImageUploadView.as_view(),name='upload-image'),
    
-    path('url/<str:url_id>/details',DetailsgetView.as_view(),name='DetailsgetView'),
-    path('url/<str:url_id>/update',DetailsView.as_view(),name='DetailsView'),
+    path('url/<str:url_id>/details', DetailsgetView.as_view(), name='DetailsgetView'),
+    path('url/<str:url_id>/update', DetailsView.as_view(), name='DetailsView'),
 
 
 
--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -81,10 +81,12 @@ class AddurlsTocartView(APIView):
         quantity = request.data.get('quantity')
         if not isinstance(quantity, int) or quantity <= 0:
             return Response({"detail": "Quantity must be a positive integer."}, status=status.HTTP_400_BAD_REQUEST)
+        
         unique_urls = UniqueURL.objects.filter(in_cart=False, user__isnull=True)[:quantity]
         if len(unique_urls) < quantity:
             return Response({"detail": "Not enough URLs to purchase"}, status=status.HTTP_404_NOT_FOUND)
-        total_price = sum(url.cost for url in unique_urls)
+        
+        total_price = sum(url.cost for url in unique_urls) 
         with transaction.atomic():
             cart_item, created = CartItem.objects.get_or_create(user=user, is_closed=False, defaults={"quantity": 0, "total_price": 0} )
             cart_item.quantity += quantity
@@ -93,6 +95,7 @@ class AddurlsTocartView(APIView):
             cart_item.unique_url.add(*unique_urls)
             UniqueURL.objects.filter(id__in=[url.id for url in unique_urls]).update(in_cart=True)
         serialized_urls = UniqueurlSerializer(cart_item.unique_url.all(), many=True).data
+        
         return Response({
             "message": "URLs added to cart successfully!",
             "cart_id": cart_item.id,
@@ -111,12 +114,15 @@ class DeleteurlView(APIView):
         quantity = request.data.get('quantity')
         if not isinstance(quantity, int) or quantity <= 0:
             return Response({"detail": "Quantity must be a positive integer."}, status=status.HTTP_400_BAD_REQUEST)
+        
         cart_item = CartItem.objects.filter(user=user, is_closed=False).first()
         if not cart_item:
             return Response({"detail": "No open cart found for this user."}, status=status.HTTP_404_NOT_FOUND)
+        
         current_quantity = cart_item.unique_url.count()
         if current_quantity < quantity:
             return Response({"detail": f"Not enough URLs in cart. Cart has only {current_quantity} URLs."}, status=status.HTTP_400_BAD_REQUEST)
+        
         urls_to_delete = cart_item.unique_url.all()[:quantity]
         with transaction.atomic():
             cart_item.unique_url.filter(id__in=[url.id for url in urls_to_delete]).delete()
@@ -125,6 +131,7 @@ class DeleteurlView(APIView):
             cart_item.total_price -= total_price_decreased
             cart_item.quantity -= len(urls_to_delete) #quantity
             cart_item.save()
+        
         serialized_urls = UniqueurlSerializer(cart_item.unique_url.all(), many=True).data
         return Response({
             "message": "URLs removed from cart successfully!",
@@ -143,7 +150,7 @@ class OpencartView(APIView):
     def get(self,request):
         user=request.user
         quantity = request.query_params.get('quantity')
-        cart_items=CartItem.objects.filter(user=user,is_closed=False)
+        cart_items=CartItem.objects.filter(user=user, is_closed=False)
         # serializer=CartitemSerializer(cart_item,many=True)
         remaining_urls=[]
         total_price=0
@@ -157,6 +164,7 @@ class OpencartView(APIView):
             urls = urls[:quantity]
         remaining_urls.extend(urls) 
         total_price += sum(url.cost for url in urls)
+        
         serialized_urls = UniqueurlSerializer(remaining_urls, many=True)
         return Response({
             "cart_id":cart_item.id,
@@ -175,6 +183,7 @@ class DetailsView(APIView):
         details_instance, _ = Details.objects.get_or_create(unique_url=unique_url)
         self.check_object_permissions(request, unique_url)
         serializer = DetailsSerializer(details_instance, data=request.data, context={'request': request}, partial=True)  
+        
         if serializer.is_valid():
             details_instance = serializer.save()
             return Response(DetailsSerializer(details_instance).data, status=status.HTTP_200_OK)
@@ -201,6 +210,7 @@ class Paymentcreateview(APIView):
         cart_id = request.data.get("cart_id")
         if not cart_id:
             return Response({"error": "Cart ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+        
         try:
             cart = CartItem.objects.get(id=cart_id, user=request.user, is_closed=False)
             total_amount = int(cart.total_price * 100)
@@ -219,11 +229,14 @@ class Paymentcreateview(APIView):
                 cancel_url=f"{settings.YOUR_DOMAIN}/api/v1/payment-cancel",
             )
             unique_url = cart.unique_url.first()
+            
             if not unique_url:
                 return Response({"error": "No UniqueURL linked to this cart."}, status=status.HTTP_400_BAD_REQUEST)
+            
             user = unique_url.user if unique_url.user else cart.user
             if not user:
                 return Response({"error": "No associated user found for this cart."}, status=status.HTTP_400_BAD_REQUEST)
+            
             payment = Payment.objects.create(
                 cart=cart,
                 user=user,
@@ -231,6 +244,7 @@ class Paymentcreateview(APIView):
                 status="PENDING",
                 checkout_id=session.id,
             )
+            
             return Response({
                 "message": "Stripe Checkout session created successfully.",
                 "session_id": session.id,
@@ -250,6 +264,7 @@ class PaymentSuccessView(APIView):
         session_id = request.GET.get('session_id')
         if not session_id:
             return Response({"error": "Session ID not provided"}, status=status.HTTP_400_BAD_REQUEST)
+        
         try:
             session = stripe.checkout.Session.retrieve(session_id)
             payment = Payment.objects.get(checkout_id=session_id)
@@ -326,8 +341,10 @@ class PaymentCancelView(APIView):
 
     def get(self, request):
         session_id = request.GET.get('session_id')   
+        
         if not session_id:
             return JsonResponse({"error": "Session ID not provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
         try:
             payment = Payment.objects.get(transaction_id=session_id)
             payment.status = "FAILED" 
@@ -338,6 +355,7 @@ class PaymentCancelView(APIView):
                 'status': payment.status,
                 'user': payment.user,
             }
+            
             return render(request, 'cancel.html', context)
             # return redirect(f"/cancel/{payment.id}/")
             # return JsonResponse({
@@ -348,7 +366,7 @@ class PaymentCancelView(APIView):
         except Payment.DoesNotExist:
             return JsonResponse({"error": "Payment not found."}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return JsonResponse({"errorSuccessfullor": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
   #feedback  
@@ -357,8 +375,12 @@ class ContactQueryView(APIView):
 
     def post(self,request):
         serializer = ContactSerializer(data=request.data)
+        
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response({"message":"your feedback has been submitted successfully"},status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message":"your feedback has been submitted successfully"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
